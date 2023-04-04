small changes, bug in backprop present

createData.py
@@ -23,8 +23,35 @@ def createData(nr_class1, nr_class2):
     class2_y = radius * np.sin(alpha)
 
     # reshape
-    class1 = np.column_stack((class1_x, class1_y))
-    class2 = np.column_stack((class2_x, class2_y))
-    
-    # shape
-    return class1, class2
+    class1 = np.column_stack((class1_x, class1_y, np.zeros((nr_class1, ))))
+    class2 = np.column_stack((class2_x, class2_y, np.ones((nr_class1, ))))
+
+    # concat data
+    data = np.vstack((class1, class2))
+
+    # shuffle data
+    np.random.shuffle(data)
+
+    return data
+
+
+def createData2(nr_class1, nr_class2):    
+    # create class 1 
+    class1_x = np.random.uniform(low = 0, high = 2, size=(nr_class1,))
+    class1_y = np.random.uniform(low = 0, high = 0.9, size=(nr_class1,))
+
+    # create class 2  
+    class2_x = np.random.uniform(low = 0, high = 2, size=(nr_class1,))
+    class2_y = np.random.uniform(low = 1.1, high = 2, size=(nr_class1,))
+
+    # reshape
+    class1 = np.column_stack((class1_x, class1_y, np.zeros((nr_class1, ))))
+    class2 = np.column_stack((class2_x, class2_y, np.ones((nr_class1, ))))
+
+    # concat data
+    data = np.vstack((class1, class2))
+
+    # shuffle data
+    np.random.shuffle(data)
+
+    return data

feedforward.py
@@ -152,7 +152,7 @@ class FeedForwardNN:
             self.layers_weights.append(current_to_next)
 
     # reset gradients to 0
-    def resetGradients(self):
+    def __resetGradients(self):
         self.gradients = []
         for i, weights in enumerate(self.layers_weights):
             self.gradients.append(np.zeros(weights.shape))